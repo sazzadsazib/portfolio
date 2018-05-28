78ced40b34cd0429ccf2d4efe6614f8f86516107
Sazzad Sazib <sazib66@gmail.com>
2018-05-29 00:41:50 +0600

    updated

--- src/component/getstarted/GetStarted.js
+++ src/component/getstarted/GetStarted.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import sazib from './assets/images/sazib-getstarted-main.svg';
+import {Link} from 'react-router-dom';
 import './assets/css/getStarted.css';
 
 class GetStarted extends Component {
@@ -7,15 +8,17 @@ class GetStarted extends Component {
     render() {
         return(
 
-               <div>
-                   <div className="container-getStarted">
-                       <img className="my-illustration" src={sazib} alt="Meh"/>
-                       <p className="getStarted-center-title">Hello</p>
-                       <p className="getStarted-center-subtitle">I am Sazzad</p>
-                       <p className="getStarted-clickhere">Click <span className="gradient-text">GET STARTED</span> to Continue</p>
-                       <button className="getStarted-continue-button gradient-button">Get Started</button>
-                   </div>
-           </div>
+            <div>
+                <div className="container-getStarted">
+                    <img className="my-illustration" src={sazib} alt="Meh"/>
+                    <p className="getStarted-center-title">Hello</p>
+                    <p className="getStarted-center-subtitle">I am Sazzad</p>
+                    <p className="getStarted-clickhere">Click <span className="gradient-text">GET STARTED</span> to Continue</p>
+                    <Link style={{color: '#ffffff'}} to={'/home'}>
+                        <button className="getStarted-continue-button gradient-button">Get Started</button>
+                    </Link>
+                </div>
+            </div>
         )
     }
 }
