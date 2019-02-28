6ff6927c6ff6267aafe928ff94ca7ea054191273
Sazzad Sazib <sazib66@gmail.com>
2019-02-28 16:08:55 +0600

    feat: adding github issue list

---
+++ src/component/Activity/ActivityCardComponent.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    card: {
+        maxWidth: '90%',
+        margin: '10px auto 20px'
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+        fontSize: 18
+    },
+};
+
+function ActivityCardComponent(props) {
+    const { classes, event } = props;
+
+    function switchCaseType(props) {
+        switch (props.type){
+            case "IssueCommentEvent":
+                return <span>
+                    Has Commented on a <a target={'_blank'} style={{fontWeight: 'bold', color: '#42a5f5'}} href={props.payload.issue.html_url}>Issue</a></span>;
+            case "PushEvent":
+                return <span>PushEvent</span>;
+            case "DeleteEvent":
+                return <span>DeleteEvent</span>;
+            case "PullRequestEvent":
+                return <span>PullRequestEvent</span>;
+            case "CreateEvent":
+                return <span>CreateEvent</span>;
+            case "PullRequestReviewCommentEvent":
+                return <span>PullRequestReviewCommentEvent</span>;
+            case "WatchEvent":
+                return <span>WatchEvent</span>;
+            case "GollumEvent":
+                return <span>GollumEvent</span>;
+            case "IssuesEvent":
+                return <span>IssuesEvent</span>;
+
+
+            default:
+                return '';
+        }
+    }
+
+    function switchCaseTypeBody(props) {
+        switch (props.type){
+            case "IssueCommentEvent":
+                return <span>{props.payload.comment.body}</span>;
+            case "PushEvent":
+                return <span>PushEvent</span>;
+            case "DeleteEvent":
+                return <span>DeleteEvent</span>;
+            case "PullRequestEvent":
+                return <span>PullRequestEvent</span>;
+            case "CreateEvent":
+                return <span>CreateEvent</span>;
+            case "PullRequestReviewCommentEvent":
+                return <span>PullRequestReviewCommentEvent</span>;
+            case "WatchEvent":
+                return <span>WatchEvent</span>;
+            case "GollumEvent":
+                return <span>GollumEvent</span>;
+            case "IssuesEvent":
+                return <span>IssuesEvent</span>;
+
+
+            default:
+                return '';
+        }
+    }
+
+
+    return (
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    <span><a style={{textDecoration: 'none', color: '#4a4a4a', fontWeight: 'bold', textTransform: 'capitalize'}}
+                       href={event.actor.url}>
+                        {event.actor.display_login}
+                    </a>
+                    &nbsp; {switchCaseType(event)}
+                    </span>
+                </Typography>
+                {/*<Typography variant="h5" component="h2">*/}
+                    {/*{switchCaseTypeBody(event)}*/}
+                {/*</Typography>*/}
+                <Typography className={classes.pos} color="textSecondary">
+                    "{switchCaseTypeBody(event)}"
+                </Typography>
+                <Typography component="p">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Learn More</Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+ActivityCardComponent.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ActivityCardComponent);
---
+++ src/component/Activity/ActivityComponent.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import NavBarMain from "../globalComponent/NavBarMain";
+import Axios from 'axios';
+import Icon from '@material-ui/core/Icon';
+import ActivityCardComponent from './ActivityCardComponent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
+class ActivityComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state= {
+            usertoFetch: "sazzadsazib",
+            dataSource: [],
+            isLoading: false,
+        };
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    fetchData() {
+        this.setState({
+            isLoading: false,
+        },()=>
+        Axios.get('https://api.github.com/users/'+this.state.usertoFetch+'/events')
+            .then(result => {
+                if (result.status === 200) {
+                   this.setState({
+                       dataSource: result.data
+                   },()=>{
+                       setTimeout(()=>this.setState({isLoading: true}),1000);
+                   });
+                }
+            }).catch((e)=>console.log(e)))
+    }
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    render() {
+        return(
+            <div>
+                <NavBarMain AppbarName={"Activity"} StalkingTime={this.props.StalkingTime} history={this.props.history} button={true} buttonName={ <Icon >autorenew</Icon>} buttonaction={()=>this.fetchData()}/>
+                {!this.state.isLoading ?
+                    <div className="center">
+                        <CircularProgress style={{color: 'primary', margin: '20vh auto 0px auto'}} thickness={4}/>
+                    </div>
+                    :
+                    <div>
+                        {this.state.dataSource.map((event,i)=>
+                            <ActivityCardComponent key={i} event={event}/>
+                        )}
+                    </div>
+                }
+            </div>
+        )
+    }
+}
+export default ActivityComponent;
