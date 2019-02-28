6d9549a82cf490fb64879169bd7e84413c6d3e5d
Sazzad Sazib <sazib66@gmail.com>
2019-03-01 00:12:36 +0600

    fix: added few more events for activity

--- src/component/Activity/ActivityCardComponent.js
+++ src/component/Activity/ActivityCardComponent.js
@@ -85,6 +85,16 @@ function ActivityCardComponent(props) {
                 return <span>Has Updated Wiki on <a style={{color: '#218cef'}} target={'_blank'} href={urlG}>{props.repo.name}</a> </span>;
             case "IssuesEvent":
                 return <span>Has {props.payload.action} an <a style={{color: '#218cef'}}  target={"_blank"} href={props.payload.issue.html_url}> Issue</a></span>;
+            case "CommitCommentEvent":
+                return 'Triggered when a commit comment is created.';
+            case "ForkEvent":
+                return "Triggered when a user forks a repository.";
+            case "LabelEvent":
+                return 'Triggered when a repositorys label is created, edited, or deleted.';
+            case "MilestoneEvent":
+                return "Triggered when a milestone is created, closed, opened, edited, or deleted.";
+            case "PublicEvent":
+                return "Triggered when a private repository is open sourced. Without a doubt: the best GitHub event.";
             default:
                 return '';
         }
@@ -190,7 +200,16 @@ function ActivityCardComponent(props) {
                     <br/>
                     <div className={classes.createdTime}>Closed at : {moment(props.created_at).fromNow()}</div>
                 </div>;
-
+            case "CommitCommentEvent":
+                return 'Triggered when a commit comment is created.';
+            case "ForkEvent":
+                return "Triggered when a user forks a repository.";
+            case "LabelEvent":
+                return 'Triggered when a repositorys label is created, edited, or deleted.';
+            case "MilestoneEvent":
+                return "Triggered when a milestone is created, closed, opened, edited, or deleted.";
+            case "PublicEvent":
+                return "Triggered when a private repository is open sourced. Without a doubt: the best GitHub event.";
 
             default:
                 return '';
@@ -218,7 +237,16 @@ function ActivityCardComponent(props) {
                 return urlG;
             case "IssuesEvent":
                 return props.payload.issue.html_url;
-
+            case "CommitCommentEvent":
+                return 'Triggered when a commit comment is created.';
+            case "ForkEvent":
+                return "Triggered when a user forks a repository.";
+            case "LabelEvent":
+                return 'Triggered when a repositorys label is created, edited, or deleted.';
+            case "MilestoneEvent":
+                return "Triggered when a milestone is created, closed, opened, edited, or deleted.";
+            case "PublicEvent":
+                return "Triggered when a private repository is open sourced. Without a doubt: the best GitHub event.";
             default:
                 return '';
         }
