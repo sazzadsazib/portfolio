49616d55a97b2fabd3f8785dfb016ab3cd9c20f4
jbshin <josephshin93@gmail.com>
2019-06-09 14:58:51 -0400

    feat(): add route for cv download page

--- src/Base.js
+++ src/Base.js
@@ -4,6 +4,7 @@ import GetStarted from "./component/getstarted/GetStarted";
 import DesktopPreview from "./component/desktop/DesktopPreview";
 // import Home from "./component/home/Home";
 import Profile from "./component/profile/Profile";
+import CVComponent from "./component/CV/CVComponent";
 import Four0Four from "./component/fourofour/Four0Four";
 import Github from "./component/Github/Github";
 // import Feed from "./component/Feed/Feed";
@@ -44,6 +45,7 @@ class Base extends Component {
                             <Route exact path='/'  render={(props)=><GetStarted StalkingTime={this.state.StalkingTime} history={props.history}/>}/>
                             <Route  exact path='/activity' render={(props)=><ActivityComponent StalkingTime={this.state.StalkingTime} history={props.history}/>}/>
                             <Route  exact path='/profile' render={(props)=><Profile StalkingTime={this.state.StalkingTime} history={props.history}/>}/>
+                            <Route  exact path='/cv' render={(props)=><CVComponent StalkingTime={this.state.StalkingTime} history={props.history}/>}/>
                             <Route  exact path='/github' render={(props)=><Github StalkingTime={this.state.StalkingTime} history={props.history} />}/>
                             <Route  exact path='/design' render={(props)=><DesignComponent StalkingTime={this.state.StalkingTime} history={props.history} />}/>
                             {/*<Route  exact path='/feed' render={(props)=><Feed StalkingTime={this.state.StalkingTime} history={props.history} />}/>*/}
